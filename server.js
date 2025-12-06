f44f5c34b33d196759c6cc93780049317141fd52
Kamran Khan <kamrangpai@gmail.com>
2025-12-06 20:01:36 +0200

    fs, url, path module and creating routers to handle request and sending responses

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
-  //   res.write("Hello World!");
-  //----------------------- we can also send status codes---------------
-  //   res.statusCode = 404; // 404- not found
-  //   res.setHeader("content-type", "text/plain");
-  res.writeHead(200, { "content-type": "text/html" });
-  console.log(req.url); //- in which terminal we hit a request
-  console.log(req.method); //- what type of request it is?
-
-  res.end("<h1>Hello World!</h1>");
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__filename);
+console.log(__dirname);
+const server = http.createServer(async (req, res) => {
+  //Creating a router
+  try {
+    if (req.method === "GET") {
+      let filePath;
+      if (req.url === "/") {
+        filePath = path.join(__dirname, "public", "home.html");
+      } else if (req.url === "/about") {
+        filePath = path.join(__dirname, "public", "about.html");
+      } else {
+        throw new Error("Not found");
+      }
+      let data = await fs.readFile(filePath);
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write(data);
+      res.end();
+    }
+  } catch (e) {
+    res.writeHead(500, { "contect-type": "text/plain" });
+    res.end("Server error");
+  }
 });
 
 server.listen(PORT, () => {
