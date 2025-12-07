d5093dad95a4211bdb4169b075af742bd9fa6468
Kamran Khan <kamrangpai@gmail.com>
2025-12-07 20:42:22 +0200

    Cleaned up and added functionality for adding new users throw POST request(method)

--- server2.js
+++ server2.js
@@ -7,28 +7,65 @@ const users = [
   { id: 3, username: "Beral" },
 ];
 
-const server = createServer((req, res) => {
-  if (req.method === "GET") {
-    if (req.url === "/api/users") {
-      res.writeHead(200, { "content-type": "application/json" });
-      res.write(JSON.stringify(users));
-      res.end();
-    } else if (req.url.match(/\/api\/users\/([0-9])+/)) {
-      const id = req.url.split("/")[3];
-      console.log(id);
-      const user = users.find((user) => user.id === parseInt(id));
-      res.writeHead(200, { "content-type": "application/json" });
-      if (user) {
-        res.write(JSON.stringify(user));
-      } else {
-        res.write(JSON.stringify({ message: "User not found" }));
-      }
-      res.end();
-    }
+// logger middleware
+const logger = (req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+};
+
+// Users handler
+const userHandler = (req, res) => {
+  res.writeHead(200, { "content-type": "application/json" });
+  res.write(JSON.stringify(users));
+  res.end();
+};
+// Users handler by ID
+const usersHandlerByID = (req, res) => {
+  const id = req.url.split("/")[3];
+  const user = users.find((user) => user.id === parseInt(id));
+  res.writeHead(200, { "content-type": "application/json" });
+  if (user) {
+    res.write(JSON.stringify(user));
   } else {
-    res.writeHead(500, { "content-type": "application/json" });
-    res.end(JSON.stringify({ message: "Server error" }));
+    res.write(JSON.stringify({ message: "User not found" }));
   }
+  res.end();
+};
+
+//creating a new user
+
+const createUserHandler = (req, res) => {
+  let body = "";
+  //listening for data
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+
+  req.on("end", () => {
+    const newUser = JSON.parse(body); // we are converting string to JS object
+    users.push(newUser);
+    res.write(JSON.stringify(newUser)); // we are converting JS object to JSON
+    res.statusCode = 201;
+    res.end();
+  });
+};
+
+const server = createServer((req, res) => {
+  logger(req, res, () => {
+    if (req.url === "/api/users" && req.method === "GET") {
+      userHandler(req, res);
+    } else if (
+      req.url.match(/\/api\/users\/([0-9])+/) &&
+      req.method === "GET"
+    ) {
+      usersHandlerByID(req, res);
+    } else if (req.url === "/api/users" && req.method === "POST") {
+      createUserHandler(req, res);
+    } else {
+      res.writeHead(500, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "Server error" }));
+    }
+  });
 });
 
 server.listen(PORT, () => {
