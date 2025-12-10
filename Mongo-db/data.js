5d9e92cbe4c70ea67404b4ae89f3c6398c290bcb
Kamran Khan <kamrangpai@gmail.com>
2025-12-11 00:00:10 +0200

    mongodb database, manipulate data, mongodb atlas, compass and vs mongosh

---
+++ Mongo-db/data.js
@@ -0,0 +1,27 @@
+post = [
+  {
+    post: 1,
+    body: "This is an example",
+    category: "News",
+    date: Date(),
+  },
+
+  {
+    post: 2,
+    body: "This is an example",
+    category: "Local",
+    date: Date(),
+  },
+  {
+    post: 3,
+    body: "This is an example",
+    category: "News",
+    date: Date(),
+  },
+  {
+    post: 4,
+    body: "This is an example",
+    category: "Timer",
+    date: Date(),
+  },
+];
