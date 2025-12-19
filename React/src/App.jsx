63734bf0cf84c8cb929326a2f11a67c3a20e4c55
Kamran Khan <kamrangpai@gmail.com>
2025-12-20 00:06:57 +0200

    Learned about how to create react routes and how to use react-router-dom and also some implimentation of props and the effective usage of logical operator

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,26 +1,26 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Homepage />} />
+      <Route path="/about" element={<h1>About Page</h1>} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+);
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero title="This is a test title" subtitle="This is a test subtitle" />
-      <HomeCards />
-
-      <JobListings />
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
